64c8e487a830971c85368179fe0b65f098764524
James White <YOUR_EMAIL@DOMAIN.COMjames.ewhitejr@yahoo.com>
2020-04-05 13:02:27 -0400

    changing table structure

--- db/migrate/20200214221201_change_ratings_to_be_decimal_in_artists.rb
+++ db/migrate/20200214221201_change_ratings_to_be_decimal_in_artists.rb
@@ -1,12 +1,12 @@
 class ChangeRatingsToBeDecimalInArtists < ActiveRecord::Migration[6.0]
   def up
-    change_column :artists do |t|
+    change_table :artists do |t|
       t.change :ratings, :float
   end
 end
 
   def down
-    change_column :artists do |t|
+    change_table :artists do |t|
     t.change :ratings, :integer
   end
 end
--- db/migrate/20200214221219_change_distance_to_be_decimal_in_artists.rb
+++ db/migrate/20200214221219_change_distance_to_be_decimal_in_artists.rb
@@ -1,11 +1,11 @@
 class ChangeDistanceToBeDecimalInArtists < ActiveRecord::Migration[6.0]
   def up
-    change_column :artists do |t|
+    change_table :artists do |t|
     t.change :distance, :float
     end
   end
   def down
-    change_column :artists do |t|
+    change_table :artists do |t|
     t.change :distance, :integer
   end
 end
