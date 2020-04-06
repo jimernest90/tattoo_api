d75a89bfaa25512765a26afac2e1a33e0f82afb6
James White <YOUR_EMAIL@DOMAIN.COMjames.ewhitejr@yahoo.com>
2020-04-06 09:10:40 -0400

    bug fixing

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,12 @@ module TatooApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+      allow do
+        origins 'https://konetink.netlify.com/', 'http://localhost:3001'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
