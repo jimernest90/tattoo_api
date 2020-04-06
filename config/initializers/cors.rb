98004db486dd1f52bfba814792d21c916e66edb5
James White <YOUR_EMAIL@DOMAIN.COMjames.ewhitejr@yahoo.com>
2020-04-06 03:07:02 -0400

    application file"

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://konetink.netlify.com'
+    origins 'https://konetink.netlify.com/'
 
     resource '*',
       headers: :any,
