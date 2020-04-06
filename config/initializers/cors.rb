65f03b06b3d550875acc2d78d58bc0f801a749c1
James White <YOUR_EMAIL@DOMAIN.COMjames.ewhitejr@yahoo.com>
2020-04-06 09:30:28 -0400

    bug fixing

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'https://konetink.netlify.com/'
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'https://konetink.netlify.com/'
 
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
