b1736326c35ce6934ca9dc242838dc1d9f9695bd
James White <YOUR_EMAIL@DOMAIN.COMjames.ewhitejr@yahoo.com>
2020-04-06 12:45:45 -0400

    bug fixing

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,3 +1,24 @@
+def cors_set_access_control_headers
+  headers['Access-Control-Allow-Origin'] = '*'
+  headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+  headers['Access-Control-Allow-Headers'] = '*'
+  headers['Access-Control-Max-Age'] = "1728000"
+end
+
+# If this is a preflight OPTIONS request, then short-circuit the
+# request, return only the necessary headers and return an empty
+# text/plain.
+
+def cors_preflight_check
+  if request.method == :options
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Max-Age'] = '1728000'
+    render :text => '', :content_type => 'text/plain'
+  end
+end
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
